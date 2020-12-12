f65403073d1fb02ca29234c6119348eb604dbc52
Taha <taha-chaoui@hotmail.com>
2020-12-12 15:16:47 +0100

    On COnstruction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "react-bootstrap/Card";
+import React from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import { Container } from "react-bootstrap";
+import { TwitterShareButton } from "react-share";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
+  const [quotes, setQuotes] = useState("");
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const shareUrl = "https://stackoverflow.com/";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to the Random Quote Machine</h1>
+      <Container id="container">
+        <Card border="dark" bg="dark" text="white">
+          <Card.Header>Quote</Card.Header>
+          <Card.Body>
+            <blockquote className="blockquote mb-0">
+              <p>{quotes.text}</p>
+              <footer className="blockquote-footer">{quotes.author}</footer>
+              <Container className="buttonTop">
+                <Button id="button">
+                  <i class="fas fa-quote-left"></i>New Quote
+                  <i class="fas fa-quote-right"></i>
+                </Button>
+                <Button>
+                  <TwitterShareButton url={shareUrl}>
+                    Tweet <i class="fab fa-twitter"></i>
+                  </TwitterShareButton>
+                </Button>
+              </Container>
+            </blockquote>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }
